4c92987f02eeeb2390a8e6a5c1562d6db302379b
Subho Karmakar <isubhokarma@gmail.com>
2019-04-28 12:36:06 +0530

    Components/ Router/ App.js Components
    New Components
    app.js return Components
    BrowerRouter: index.js/Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
-  return (
-    <React.Fragment>
-      Hello, World!
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Navbar />
+			<ProductList />
+			<Details />
+			<Cart />
+			<Default />
+		</React.Fragment>
+	);
 }
 
 export default App;
