cc66ea8639eab7c019abd7954f4e658c44407fea
Subho Karmakar <isubhokarma@gmail.com>
2019-04-28 16:50:25 +0530

    Components called/ Data
     called: switch with all components/ non components: default
    Data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Hello Navbar</h3>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+				<Link to="/">
+					<img src={logo} alt="store" className="navbar-brand" />
+				</Link>
+				<ul class="navbar-nav align-items-center">
+					<li className="nav-item ml-5">
+						<Link to="/" className="nav-Link">
+							products
+						</Link>
+					</li>
+				</ul>
+				<Link to="/cart" className="ml-auto">
+					<button>
+						<i className="fas fa-cart-plus" />
+						my cart
+					</button>
+				</Link>
+			</nav>
+		);
+	}
 }
